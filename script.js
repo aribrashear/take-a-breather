c0b1f73f0120c22ed0e739f9305a31910ce35649
aribrashear <aribrashear@gmail.com>
2023-02-17 21:11:48 -0800

    Function is running on button click, but is not repeating properly.

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 "use strict"
-
 const container = document.getElementById("container")
 const text = document.getElementById("text")
 const pointerContainer = document.getElementById("pointerContainer")
-// 8500  milliseconds = 8.5 seconds
+
+// 8500 milliseconds = 8.5 seconds
 const totalTime = 9500
 const breatheTime = (totalTime / 5) * 2
 const holdTime = totalTime / 5
 let animationRunning = false
+let timeoutId
 
-// Immediately running the function on page load.
+function toggleAnimation() {
+  animationRunning = !animationRunning
+  if (animationRunning) {
+    toggleBreatheAnimation()
+  } else {
+    clearTimeout(timeoutId)
+  }
+}
 
-function setStyles() {
-  pointerContainer.style.animation = "rotate 9.5s linear forwards infinite"
+function toggleBreatheAnimation() {
+  breatheAnimation()
+  timeoutId = setTimeout(() => {
+    toggleBreatheAnimation()
+  }, breatheTime + holdTime)
 }
 
 function breatheAnimation() {
-  animationRunning = true
   text.innerHTML = "Breathe In."
   container.className = "container grow"
   pointerContainer.style.animation = "rotate 9.5s linear forwards infinite"
 
-  // Takes in a function to be executed, and a timer that will count (in ms) down to execute.
   setTimeout(() => {
     text.innerText = "Hold."
 
@@ -31,8 +40,55 @@ function breatheAnimation() {
     }, holdTime)
   }, breatheTime)
 }
+// const container = document.getElementById("container")
+// const text = document.getElementById("text")
+// const pointerContainer = document.getElementById("pointerContainer")
+// // 8500  milliseconds = 8.5 seconds
+// const totalTime = 9500
+// const breatheTime = (totalTime / 5) * 2
+// const holdTime = totalTime / 5
+// let animationRunning = false
 
-while (animationRunning) {
-  // Runs the function every 7 seconds (or for the specified time).
-  setInterval(breatheAnimation, totalTime)
-}
+// // Immediately running the function on page load.
+
+// function setStyles() {
+//   pointerContainer.style.animation = "rotate 9.5s linear forwards infinite"
+// }
+// function toggleAnimation() {
+//   animationRunning = !animationRunning
+// }
+
+// let timeoutId
+
+// function toggleBreatheAnimation(toggle) {
+//   animationRunning = toggle
+
+//   if (toggle) {
+//     breatheAnimation()
+//     timeoutId = setTimeout(() => {
+//       toggleBreatheAnimation(animationRunning)
+//     }, breatheTime + holdTime)
+//   } else {
+//     clearTimeout(timeoutId)
+//   }
+// }
+
+// function breatheAnimation() {
+//   text.innerHTML = "Breathe In."
+//   container.className = "container grow"
+//   pointerContainer.style.animation = "rotate 9.5s linear forwards infinite"
+
+//   // Takes in a function to be executed, and a timer that will count (in ms) down to execute.
+//   setTimeout(() => {
+//     text.innerText = "Hold."
+
+//     setTimeout(() => {
+//       text.innerText = "Breathe Out."
+//       container.className = "container shrink"
+//     }, holdTime)
+//   }, breatheTime)
+// }
+// while (animationRunning) {
+//   // Runs the function every 7 seconds (or for the specified time).
+//   setInterval(breatheAnimation, totalTime)
+// }
