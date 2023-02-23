3574a0dbb880cd32f0fbade16be6e5970e79c54c
aribrashear <aribrashear@gmail.com>
2023-02-22 20:42:57 -0800

    Reset code to original state to fix behavior.

--- script.js
+++ script.js
@@ -1,95 +1,25 @@
 "use strict"
-
 const container = document.getElementById("container")
 const text = document.getElementById("text")
-const pointerContainer = document.getElementById("pointerContainer")
 
-// 8500 milliseconds = 8.5 seconds
 const totalTime = 9500
 const breatheTime = (totalTime / 5) * 2
 const holdTime = totalTime / 5
-let animationRunning = false
-let timeoutId
 
-function toggleAnimation() {
-  animationRunning = !animationRunning
-  if (animationRunning) {
-    toggleBreatheAnimation()
-  } else {
-    clearTimeout(timeoutId)
-  }
-}
+breathAnimation()
 
-function toggleBreatheAnimation() {
-  breatheAnimation()
-  timeoutId = setTimeout(() => {
-    toggleBreatheAnimation()
-  }, breatheTime + holdTime)
-}
-
-function breatheAnimation() {
-  text.innerHTML = "Breathe In."
+function breathAnimation() {
+  text.innerText = "Breathe In!"
   container.className = "container grow"
-  pointerContainer.style.animation = "rotate 9.5s linear forwards infinite"
 
   setTimeout(() => {
-    text.innerText = "Hold."
+    text.innerText = "Hold"
 
     setTimeout(() => {
-      text.innerText = "Breathe Out."
+      text.innerText = "Breathe Out!"
       container.className = "container shrink"
     }, holdTime)
   }, breatheTime)
 }
-// const container = document.getElementById("container")
-// const text = document.getElementById("text")
-// const pointerContainer = document.getElementById("pointerContainer")
-// // 8500  milliseconds = 8.5 seconds
-// const totalTime = 9500
-// const breatheTime = (totalTime / 5) * 2
-// const holdTime = totalTime / 5
-// let animationRunning = false
 
-// // Immediately running the function on page load.
-
-// function setStyles() {
-//   pointerContainer.style.animation = "rotate 9.5s linear forwards infinite"
-// }
-// function toggleAnimation() {
-//   animationRunning = !animationRunning
-// }
-
-// let timeoutId
-
-// function toggleBreatheAnimation(toggle) {
-//   animationRunning = toggle
-
-//   if (toggle) {
-//     breatheAnimation()
-//     timeoutId = setTimeout(() => {
-//       toggleBreatheAnimation(animationRunning)
-//     }, breatheTime + holdTime)
-//   } else {
-//     clearTimeout(timeoutId)
-//   }
-// }
-
-// function breatheAnimation() {
-//   text.innerHTML = "Breathe In."
-//   container.className = "container grow"
-//   pointerContainer.style.animation = "rotate 9.5s linear forwards infinite"
-
-//   // Takes in a function to be executed, and a timer that will count (in ms) down to execute.
-//   setTimeout(() => {
-//     text.innerText = "Hold."
-
-//     setTimeout(() => {
-//       text.innerText = "Breathe Out."
-//       container.className = "container shrink"
-//     }, holdTime)
-//   }, breatheTime)
-// }
-// while (animationRunning) {
-//   // Runs the function every 7 seconds (or for the specified time).
-//   setInterval(breatheAnimation, totalTime)
-// }
+setInterval(breathAnimation, totalTime)
